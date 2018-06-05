5f2a5520a06e6c0a4aa6c91378dbb85d21552ed7
sunghwanson <zldejagkcm@naver.com>
2018-06-05 17:55:33 +0900

    test

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from tensorflow.contrib import legacy_seq2seq
 
 import numpy as np
 
-#수정 테스트 dffeeeee
+
 class Model():
     def __init__(self, args, training=True):
         self.args = args
@@ -25,7 +25,7 @@ class Model():
 
         cells = []
         for _ in range(args.num_layers):
-            cell = cell_fn(args.rnn_size)
+            cell = cell_fn(args.rnn_size)   #여기서의 rnn_size는 out_put layer의 크기라고 볼수 있다??
             if training and (args.output_keep_prob < 1.0 or args.input_keep_prob < 1.0):
                 cell = rnn.DropoutWrapper(cell,
                                           input_keep_prob=args.input_keep_prob,
@@ -34,67 +34,68 @@ class Model():
 
         self.cell = cell = rnn.MultiRNNCell(cells, state_is_tuple=True)
 
-        self.input_data = tf.placeholder(
+        self.input_data = tf.placeholder(   # embedding을 위함
             tf.int32, [args.batch_size, args.seq_length])
-        self.targets = tf.placeholder(
+        self.targets = tf.placeholder(  #정답 값을 가지고 있는 데이터
             tf.int32, [args.batch_size, args.seq_length])
-        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+        self.initial_state = cell.zero_state(args.batch_size, tf.float32) #3*2*60*700
 
         with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w",
+            softmax_w = tf.get_variable("softmax_w",    #w개수만큼 생성
                                         [args.rnn_size, args.vocab_size])
-            softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
+            softmax_b = tf.get_variable("softmax_b", [args.vocab_size]) #b개수만큼 생성
 
-        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-        inputs = tf.nn.embedding_lookup(embedding, self.input_data)
+        embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])  #train을 통해 char 데이터에 해당하는 수치들을 갱신하면서 embedding table을 갱신하고
+        inputs = tf.nn.embedding_lookup(embedding, self.input_data) #batch_size 만큼의 데이터를 각각의 데이터에 해당하는 embedding vec로 변환해서 inputs변수에 저장 60*100*700
 
         # dropout beta testing: double check which one should affect next line
         if training and args.output_keep_prob:
             inputs = tf.nn.dropout(inputs, args.output_keep_prob)
 
-        inputs = tf.split(inputs, args.seq_length, 1)
-        inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+        inputs = tf.split(inputs, args.seq_length, 1)  #60*100*(700) => 100*60*1*(700)
+        inputs = [tf.squeeze(input_, [1]) for input_ in inputs] #100*60*1*(700) => 100*60*(700)
 
-        def loop(prev, _):
+        def loop(prev, _):  #train이 아니고 과거 훈련 모델로 값을 출력하고 싶은경우 더이상 bp를 할 필요가 없기에 설정해주는 것 같음.
             prev = tf.matmul(prev, softmax_w) + softmax_b
             prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
             return tf.nn.embedding_lookup(embedding, prev_symbol)
 
         outputs, last_state = legacy_seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if not training else None, scope='rnnlm')
-        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size])
+        #outputs는 100*60*700 last_state는 3*2*60*700 inputs를 lstm의 입력으로 전달
+        output = tf.reshape(tf.concat(outputs, 1), [-1, args.rnn_size]) #6000*700
 
 
-        self.logits = tf.matmul(output, softmax_w) + softmax_b
-        self.probs = tf.nn.softmax(self.logits)
-        loss = legacy_seq2seq.sequence_loss_by_example(
-                [self.logits],
-                [tf.reshape(self.targets, [-1])],
-                [tf.ones([args.batch_size * args.seq_length])])
-        self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
+        self.logits = tf.matmul(output, softmax_w) + softmax_b #6000*700 x 700*voca_size + voca_size
+        self.probs = tf.nn.softmax(self.logits) #가중치 및 바이어스 값들 계산 및 softmax
+        loss = legacy_seq2seq.sequence_loss_by_example( #rnn에 특화된 loss fanction, loss(erorr)를 한번에 계산
+                [self.logits],  #Y에 해당하는 예측값
+                [tf.reshape(self.targets, [-1])],   #정답 레이블
+                [tf.ones([args.batch_size * args.seq_length])]) #가중치
+        self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length #최종 cost계산
         with tf.name_scope('cost'):
             self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
-        self.final_state = last_state
-        tvars = tf.trainable_variables()
+        self.final_state = last_state   #cell의 최종 상태
+        tvars = tf.trainable_variables() #가시적인 그래프 생성
         grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
-                args.grad_clip)
+                args.grad_clip) #그래디언트의 크기를 줄이기 위함
         with tf.name_scope('optimizer'):
-            optimizer = tf.train.AdamOptimizer()
-        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
+            optimizer = tf.train.AdamOptimizer()    #adamoptimizer 사용
+        self.train_op = optimizer.apply_gradients(zip(grads, tvars))    #그래디언트 클리프를 적용하여 학습하도록 함.
 
         # instrument tensorboard
         tf.summary.histogram('logits', self.logits)
         tf.summary.histogram('loss', loss)
         tf.summary.scalar('train_loss', self.cost)
 
-    def sample(self, sess, chars, vocab, num=200, prime=' ', sampling_type=1):
+    def sample(self, sess, chars, vocab, num=200, prime=' ', sampling_type=1):  #학습된 모델로 출력하기 위함.
         """
-        prime: starting character sequence. 
+        prime: starting character sequence.
         sampling_type: 0 to use max at each timestep, 1 to sample at each timestep, 2 to sample on spaces
         """
-        state = sess.run(self.cell.zero_state(1, tf.float32))
+        state = sess.run(self.cell.zero_state(1, tf.float32))   #state값에 0 채우기
         print('Generate sample start with:', prime)
-        # make rnn state by feeding in prime sequence. 
-        for char in prime[:-1]:
+        # make rnn state by feeding in prime sequence.
+        for char in prime[:-1]: #prime(seed 문자열)을 한글자씩 입력
             print('put this to rnn to make state:', char)
             x = np.zeros((1, 1)) # 1x1 matrix
             x[0, 0] = vocab[char]
@@ -110,7 +111,7 @@ class Model():
         char = prime[-1]
         for n in range(num):
             x = np.zeros((1, 1))
-            x[0, 0] = vocab[char]
+            x[0, 0] = vocab[char]   #key 값(vocab 글자)에 의한 index값
             feed = {self.input_data: x, self.initial_state: state}
             [probs, state] = sess.run([self.probs, self.final_state], feed)
             p = probs[0]
--- utils.py
+++ utils.py
@@ -28,14 +28,14 @@ class TextLoader():
     def preprocess(self, input_file, vocab_file, tensor_file):
         with codecs.open(input_file, "r", encoding=self.encoding) as f:
             data = f.read()
-        counter = collections.Counter(data)
-        count_pairs = sorted(counter.items(), key=lambda x: -x[1])
-        self.chars, _ = zip(*count_pairs)
-        self.vocab_size = len(self.chars)
-        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        counter = collections.Counter(data) #data를 char 기준으로 카운트 리스트 생성
+        count_pairs = sorted(counter.items(), key=lambda x: -x[1])  #카운트 리스트 1번째(카운트 횟수)기준으로 소팅
+        self.chars, _ = zip(*count_pairs)   #count_pairs 의 0번째 인자들(글자)는 chars에 횟수 인자들은 _에 저장
+        self.vocab_size = len(self.chars) #추출한 글자의 종류 개수
+        self.vocab = dict(zip(self.chars, range(len(self.chars)))) #chars에 index부여
         with open(vocab_file, 'wb') as f:
-            cPickle.dump(self.chars, f)
-        self.tensor = np.array(list(map(self.vocab.get, data)))
+            cPickle.dump(self.chars, f) #chars를 파일로 저장
+        self.tensor = np.array(list(map(self.vocab.get, data))) #data의 글자들을 vocab index 값들로 치환
         np.save(tensor_file, self.tensor)
 
     def load_preprocessed(self, vocab_file, tensor_file):
@@ -49,27 +49,29 @@ class TextLoader():
 
     def create_batches(self):
         self.num_batches = int(self.tensor.size / (self.batch_size *
-                                                   self.seq_length))
+                                                   self.seq_length))    #공백을 포함한 data문자 수/(60*100) @back propagation의 횟수와 동일
 
         # When the data (tensor) is too small,
         # let's give them a better error message
         if self.num_batches == 0:
             assert False, "Not enough data. Make seq_length and batch_size small."
 
-        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length]
+        self.tensor = self.tensor[:self.num_batches * self.batch_size * self.seq_length] #num_batches값에 딱 떨어지게 잘라냄
         xdata = self.tensor
         ydata = np.copy(self.tensor)
         ydata[:-1] = xdata[1:]
-        ydata[-1] = xdata[0]
-        self.x_batches = np.split(xdata.reshape(self.batch_size, -1),
-                                  self.num_batches, 1)
+        ydata[-1] = xdata[0]    #ydata의 데이터를 한칸 밀어서 생성
+        self.x_batches = np.split(xdata.reshape(self.batch_size, -1),   #xdata.len/batch_size 의 원소의 개수로 batch_size로 분할
+                                  self.num_batches, 1)  #ex)12000개의 글자 data라면, reshape로 60*200의 데이터를 생성하고
+                                                        #split으로 2*60*100의 데이터로 만듬 즉 한덩어리씩 2번 bp를 진행
         self.y_batches = np.split(ydata.reshape(self.batch_size, -1),
                                   self.num_batches, 1)
 
-    def next_batch(self):
+    def next_batch(self):   #배치 포인터
         x, y = self.x_batches[self.pointer], self.y_batches[self.pointer]
         self.pointer += 1
         return x, y
 
     def reset_batch_pointer(self):
         self.pointer = 0
+
